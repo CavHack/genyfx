be0ae8d71d1ea2d2d4cfc0d2236726f163f9e48a
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-10-21 22:18:08 -0700

    substituting local outliers
    A method of substituting local outliers for NA followed by standard methods of processing NA is suitable for such samples.

--- preDat.R
+++ preDat.R
@@ -21,3 +21,24 @@ evalq(ggplot(dataSetClean, aes(x = factor(0),
 evalq({dataSetClean$ftlm -> x  
   out.ftlm <- x[!x %in% boxplot.stats(x)$out]}, 
   env)
+#-----
+evalq({dataSetClean$ftlm -> x 
+  out.ftlm1 <- x[x > quantile(x, .25) - 1.5*IQR(x) & 
+          x < quantile(x, .75) + 1.5*IQR(x)]},
+  env)
+evalq(all.equal(out.ftlm, out.ftlm1), env)
+nrow(env$dataSetClean) - length(env$out.ftlm)
+#-------Ris10-------------
+boxplot(env$out.ftlm, main = "ftlm  without outliers", 
+        boxwex = 0.5)
+#---remove--outliers------------------
+remove_outliers <- function(x, na.rm = TRUE, ...) {
+  qnt <- quantile(x, probs = c(.25, .75), 
+                  na.rm = na.rm, ...)
+  H <- 1.5 * IQR(x, na.rm = na.rm)
+  y <- x
+  y[x < (qnt[1] - H)] <- NA
+  y[x > (qnt[2] + H)] <- NA
+  y
+}
+#----------
