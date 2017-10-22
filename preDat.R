7f8b5d450d9f4ae287c1c34073e04eb0648b8aad
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-10-21 22:15:27 -0700

    Removing Outliers
    The outliers must be deleted if they are caused by the data entry, processing data or the number of outliers is very small (only when identifying statistical variable metrics).
    
    Data of one variable (ftlm, for instance) can be extracted without outliers

--- preDat.R
+++ preDat.R
@@ -9,3 +9,15 @@ env$preProClean$method$remove
 #[1] "v.rbci"
 colnames(env$dataSetClean)
 ##-------outlier-------------------------------
+#-------boxplot---Ris9---------
+evalq(ggplot(dataSetClean, aes(x = factor(0), 
+                               y = ftlm,
+                               color = 'red')) + 
+        geom_boxplot() + xlab("") + 
+        scale_x_discrete(breaks = NULL) + 
+        coord_flip(),
+      env)
+#------------------------
+evalq({dataSetClean$ftlm -> x  
+  out.ftlm <- x[!x %in% boxplot.stats(x)$out]}, 
+  env)
