7ad5f2c8ffa43e32d816c4cfaf0f1700f35ad822
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-10-22 20:05:11 -0700

    Getting rid of right skewness
    iterative arithmetic log taking.

--- featTrans.R
+++ featTrans.R
@@ -4,3 +4,14 @@ require(GGally)
 require(ggvis)
 require(discretization)
 require(caret)
+require(pipeR)
+require(funModeling)
+require(lubridate)
+
+#Get rid of right skewness of the x data set
+#take log base 2 and then remove outliers
+#for values smaller than 1 add 1 and take log
+
+evalq({x.ln <- apply(x, 2, function(x) log2(x+1))
+	    sk.ln <- skewness(x.ln)},
+	    env)
