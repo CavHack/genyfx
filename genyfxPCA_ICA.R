afb1d5dbc2ae8b489f55f36cd0353a405f76cf0d
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-10-24 23:11:41 -0700

    fastICA
    The caret package for calculating ICA uses the fastICA package with the following algorithm:
    after the initial matrix is centered (normalization of rows is possible), we obtain the Х matrix
    then the Х matrix is projected into the directions of the principle components and we obtain PCA = X * K as a result
    after that, PCA is multiplied by the unmixing matrix and we obtain independent components ICA = X * K * W

--- genyfxPCA_ICA.R
+++ genyfxPCA_ICA.R
@@ -13,3 +13,23 @@ evalq({
 
 str(env$prePCA)
 str(env$preICA)
+
+#---PCA/ICA----
+require(magrittr)
+evalq({
+	pca <- predict(prePCA, x.cap)
+	ica <- predict(preICA, x.cap)
+	pca1 <- ((x.cap %>% scale(prePCA$mean, prePCA$std))
+
+%*%
+		prePCA$rotation)
+	ica1 <- ((x.cap %>% scale(preICA$mean, preICA$std))
+%*%
+	preICA$ica$K) %*% preICA$ica$W
+colnames(ica1) <- colnames(ica1, do.Null = FALSE, prefix = 'ICA')
+} env)
+
+evalq(all_equal(pca, pca1), env)
+# [1] TRUE
+evalq(all_equal(ica, ica1), env)
+# [1] TRUE
