52bdecd87db97b5a2cf68c4581c6a654baf01937
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-10-24 21:50:45 -0700

    Feature Selection Canonical Transform
    tanh via ln sinusoidal transform

--- featSelect.R
+++ featSelect.R
@@ -71,6 +71,128 @@ evalq({
 
 }, env)
 
+#------sintrans--------------
+evalq({
+  DTSin <- list()
+  foreach(i = 1:3) %do% {
+    DTcap[[i]] %>% 
+      apply(., 2, function(x) sin(2*pi*x)) %>%
+      as.data.frame() %>%
+      cbind(., Class = DT[[i]]$Class)
+  } -> DTSin
+},
+env)
+#------tanhTrans----------
+evalq({
+  DTTanh <- list()
+  foreach(i = 1:3) %do% {
+    DTcap[[i]] %>% 
+      apply(., 2, function(x) tanh(x)) %>%
+      as.data.frame() %>%
+      cbind(., Class = DT[[i]]$Class)
+  } -> DTTanh
+},
+env)
+#------normalize-----------
+evalq(
+  {
+#define parameters of normalization
+    preProcess(DT$train, method = "spatialSign") -> preproc 
+    list(train = predict(preproc, DT$train), 
+         val = predict(preproc, DT$val),
+         test = predict(preproc, DT$test)
+    ) -> DTn
+  }, 
+  env) 
+#--ln---
+evalq(
+  {
+    preProcess(DTLn[[1]], method = "spatialSign") -> preprocLn 
+    list(train = predict(preprocLn, DTLn[[1]]), 
+         val = predict(preprocLn, DTLn[[2]]),
+         test = predict(preprocLn, DTLn[[3]])
+    ) -> DTLn.n
+  }, 
+  env)
+#---sin---
+evalq(
+  {
+    preProcess(DTSin[[1]], method = "spatialSign") -> preprocSin 
+    list(train = predict(preprocSin, DTSin[[1]]), 
+         val = predict(preprocSin, DTSin[[2]]),
+         test = predict(preprocSin, DTSin[[3]])
+    ) -> DTSin.n
+  }, 
+  env)
+#-----tanh-----------------
+evalq(
+  {
+    preProcess(DTTanh[[1]], method = "spatialSign") -> preprocTanh 
+    list(train = predict(preprocTanh, DTTanh[[1]]), 
+         val = predict(preprocTanh, DTTanh[[2]]),
+         test = predict(preprocTanh, DTTanh[[3]])
+    ) -> DTTanh.n
+  }, 
+  env)
+##------discretize----------
+#--------preCut---------------------
+require(pipeR)
+require(discretization)
+evalq(
+  #require(pipeR) time !!!
+  pipeline({
+    DT$train
+    select(-Data)
+    as.data.frame()
+    mdlp() 
+  }) -> mdlp.train, 
+  env)
+#-------cut_opt----------
+evalq(
+  {
+    DTd <- list()
+    mdlp.train$cutp %>% 
+# define the columns that have to be discretized.
+      lapply(., function(x) is.numeric(x)) %>%
+      unlist -> idx   # bool
+    #----train-----------------
+    mdlp.train$Disc.data[ ,idx] -> DTd$train 
+    #---test------------
+    DT$test %>% 
+      select(-c(Data, Class)) %>%
+      as.data.frame() -> test.d
+    #rearrange data according to calculated ranges
+    foreach(i = 1:length(idx), .combine = 'cbind') %do% {
+      if (idx[i]) {
+        findInterval(test.d[ ,i], 
+        vec = mdlp.train$cutp[[i]],
+        rightmost.closed = FALSE, 
+        all.inside = F,
+        left.open = F)
+        }
+    } %>% as.data.frame() %>% add(1) %>%
+      cbind(., DT$test$Class) -> DTd$test
+    colnames(DTd$test) <- colnames(DTd$train)
+    #-----val-----------------
+    DT$val %>% 
+      select(-c(Data, Class)) %>%
+      as.data.frame() -> val.d
+    foreach(i = 1:length(idx), .combine = 'cbind') %do% {
+      if (idx[i]) {
+        findInterval(val.d[ ,i], 
+        vec = mdlp.train$cutp[[i]],
+        rightmost.closed = FALSE, 
+        all.inside = F,
+        left.open = F)
+        }
+    } %>% as.data.frame() %>% add(1) %>%
+      cbind(., DT$val$Class) -> DTd$val 
+    colnames(DTd$val) <- colnames(DTd$train)
+    rm(test.d, val.d)
+  }, 
+  env
+)
+
 
 
 }})
