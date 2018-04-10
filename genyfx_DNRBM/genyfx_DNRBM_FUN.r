9b973930ff1224110e041278d45abc4f78f3028a
Draco <kw@thevenire.com>
2018-04-10 16:47:12 -0400

    oscillator setup
    we used 11 indicators (all oscillators: ADX, aroon, ATR, CCI, chaikinVolatility, CMO, MACD, RSI, stoch, SMI, volatility). Several variables were selected from some indicators. This way we have formed the input set of 17 variables. Let's take quotes from the last 6000 bars on EURUSD, М30 as at 14.02.16, and calculate indicator values using the In() function.

--- genyfx_DNRBM/genyfx_DNRBM_FUN.r
+++ genyfx_DNRBM/genyfx_DNRBM_FUN.r
@@ -23,7 +23,34 @@ In <- function(p = 16) {
    ar <- aroon(price[,c('High', 'Low')], n=p) %>%
    extract(,3)
    atr <- ATR(price, n=p, maType = "EMA") %>%
+   extract(, 1:2)
+   cci <- CCI(price[ ,2:4], n=p)
+   chv <- chaikinVolatility(price[ ,2:4], n=p)
+   cmo <- CMO(price[ ,'Med'], n=p)
+   macd <- MACD(price[ ,'Med'], 12, 26, 9) %>%
+   as.data.frame() %>%
+   mutate(., vsig = signal %>%
+   	     diff %>% c(NA, .) %>% multiply_by((10)) %>%
 
 
-
+   transmute(., sign = signal, vsig) %>%
+	     as.matrix()
+   rsi <- RSI(price[, 'Med'], n=p)
+   stoh <- stoch(price[, 2:4], nFastK = p,
+   	   	 nFastD = 3, nSlowD = 3,
+		 maType = "EMA") %>%
+		 as.data.frame() %>%
+		 mutate(., oscK = fastK - fastD) %>%
+		 transmute(., slowD, oscK) %>%
+		 as.matrix()
+    smi <- SMI(price[ ,2:4],n = p, nFast = 2, 
+             nSlow = 25, nSig = 9)
+    kst <- KST(price[ ,4])%>% as.data.frame() %>% 
+				mutate(., oscKST = kst - signal) %>%
+				select(.,oscKST) %>% as.matrix()
+  In <- cbind(adx, ar, atr, cci, chv, cmo, macd, 
+              rsi, stoh, smi, kst)
+  return(In)
+}
+		 
 }
