65b502c9838fec1817e8d19883ca226e2afc0f40
Draco <kw@thevenire.com>
2018-04-14 01:25:50 -0400

    Imputate and Prune
    Data Test Topology

---
+++ genyfx_ENSEMBLE/genyfx_Optim_ENSEMBLE.R
@@ -0,0 +1,44 @@
+library(anytime)
+library(rowr)
+library(elmNN)
+library(rBayesianOptimization)
+library(foreach)
+library(magrittr)
+library(clusterSim)
+library(doRNG)
+
+#----prepare-----
+evalq({
+	dt <- PrepareData(Data, Open, High, Low, Close, Volume)
+	DT <- SplitData(dt, 4000, 1000, 500, 250, start = 1)
+	pre.outl <- PreOutlier(DT$pretrain)
+	DTcap <- CappingData(DT, impute = T, fill = T, dither = F, pre.outl = pre.outl)
+	preproc <- PreNorm(DTcap, meth = meth)
+	DTcap.n <- NormData(DTcap, preproc = preproc)
+	#---1--Data X -----------
+	list(
+			pretrain = list(
+			x = DTcap.n$pretrain %>% dplyr::select(-c(Data, Class)) %>%
+			as.data.frame(),
+			y = DTcap.n$pretrain$class %>% as.numeric() %>% subtract(1)),
+			
+			train = list(
+			x = DTcap.n$train %>% dlpyr::select(-c(Data, Class)) %>%
+			as.data.frame(),
+			y = DTcap.n$train$Class %>% as.numeric() %>% subtract(1)),
+			test = list(
+      		x = DTcap.n$val %>% dplyr::select(-c(Data, Class)) %>% as.data.frame(),
+    	    y = DTcap.n$val$Class %>% as.numeric() %>% subtract(1) 
+   		 ),
+   			 test1 = list(
+    		  x = DTcap.n$test %>% dplyr::select(-c(Data, Class)) %>% as.data.frame(), 
+    		  y = DTcap.n$test$Class %>% as.numeric() %>% subtract(1) 
+    )
+  ) -> X
+	
+	
+	
+	)
+	
+	
+})
