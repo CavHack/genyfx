1273f61acae46fe4f7a6d65ecca9a582b2c39389
Draco <kw@thevenire.com>
2018-04-13 23:10:51 -0400

    Confusion Matrix deduction
    Penalize col and row sums

--- genyfx_ENSEMBLE/genyfx_Ensemble.R
+++ genyfx_ENSEMBLE/genyfx_Ensemble.R
@@ -10,7 +10,56 @@ Evaluate <- function(actual=NULL, predicted=NULL, cm=NULL) {
 		predicted = factor(predicted, levels = levels(f))
 		cm = as.matrix(table(Actual = actual, Predicted = predicted))
 	}
+	
+	n = sum(cm) #number of instances
+	nc = nrow(cm) #number of classes
+	diag = diag(cm) #number of correctly classified instances per class
+	rowsums = apply(cm, 1, sum) #number of instances per class
+	colsums = apply(cm, 2, sum) #number of predictions per class
+	p = rowsums / n #distribution of instances over the classes
+	q = colsums / n #distribution of instances over the predicted classes
+	
+	#accuracy
+	accuracy = sum(diag) / n
+	
+	#per class
+	recall = diag / rowsums
+	precision = diag / colsums
+	f1 = 2 * precision * recall / (precision + recall)
+	
+	#macro
+	macroPrecision = mean(precision)
+	macroRecall = mean(recall)
+	macroF1 = mean(f1)
+	
+	#1-vs-all matrix
+	oneVsAll = lapply(1:nc,
+						function(i) {
+							v = c(cm[i,i],
+								  rowsums[i] - cm[i,i],
+								  colsums[i] - cm[i,i],
+								  n - rowsums[i] - colsums[i] + cm[i,i]);
+								  return(matrix(v, nrow =2, byrow= T))})
+								  
+	
+	
 }
 
 
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
